69f623a89ac4c43869c2d6ecd1c0ec897e1c26c8
coderio475-collab <coderio475@gmail.com>
2026-08-17 13:22:01 -0300

    Update UsuarioController.php

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -1,1 +1,18 @@
+<?php
 
+require_once __DIR__ . "/../models/Usuario.php";
+
+class UsuarioController
+{
+    private $usuario;
+
+    public function __construct()
+    {
+        $this->usuario = new Usuario();
+    }
+
+    public function listar()
+    {
+        return $this->usuario->obtenerTodos();
+    }
+}
