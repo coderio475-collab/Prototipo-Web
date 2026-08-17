3fb20795f659b2bc9faa00ea81cb15b953326e8e
coderio475-collab <coderio475@gmail.com>
2026-08-17 13:21:29 -0300

    Update Usuario.php

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,1 +1,21 @@
+<?php
 
+require_once __DIR__ . "/../../database/conexion.php";
+
+class Usuario
+{
+    private $conexion;
+
+    public function __construct()
+    {
+        $this->conexion = (new Conexion())->conectar();
+    }
+
+    public function obtenerTodos()
+    {
+        $sql = "SELECT * FROM usuarios";
+        $consulta = $this->conexion->query($sql);
+
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
