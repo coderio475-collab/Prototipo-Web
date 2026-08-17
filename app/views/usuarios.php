dc68935dd589cc8aceef77bc0ae4fe58551eab31
coderio475-collab <coderio475@gmail.com>
2026-08-17 13:22:42 -0300

    Update usuarios.php

--- app/views/usuarios.php
+++ app/views/usuarios.php
@@ -1,1 +1,31 @@
+<?php
 
+require_once __DIR__ . "/../controllers/UsuarioController.php";
+
+$controller = new UsuarioController();
+$usuarios = $controller->listar();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Usuarios</title>
+</head>
+
+<body>
+
+    <h1>Lista de usuarios</h1>
+
+    <?php foreach ($usuarios as $usuario): ?>
+
+        <p>
+            <?php echo $usuario['Nombre']; ?>
+            <?php echo $usuario['Apellido']; ?>
+        </p>
+
+    <?php endforeach; ?>
+
+</body>
+</html>
