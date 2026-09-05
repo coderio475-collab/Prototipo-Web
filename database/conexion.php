ab6b8873e0c17a306f4f00fbd2eafcc804ea9930
coderio475-collab <coderio475@gmail.com>
2026-09-05 16:02:42 -0300

    Add files via upload

--- database/conexion.php
+++ database/conexion.php
@@ -5,7 +5,7 @@ class Conexion
     private $host = "localhost";
     private $usuario = "root";
     private $password = "";
-    private $bd = "riocode";
+    private $bd = "Prototipo-Web";
 
     public function conectar()
     {
