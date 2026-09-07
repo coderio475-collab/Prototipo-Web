bea8843160c46f49f26cec811e36fb468b350bda
Brian <coderio475@gmail.com>
2026-09-07 16:40:30 -0300

    fix

--- database/conexion.php
+++ database/conexion.php
@@ -5,7 +5,7 @@ class Conexion
     private $host = "localhost:3306";
     private $usuario = "root";
     private $password = "";
-    private $bd = "Prototipo-Web";
+    private $bd = "riocode";
 
     public function conectar()
     {
